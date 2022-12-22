2caedbd7f3cc3378c1d2e8bc5e7c1b28acc0721b
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-22 19:40:40 +0200

    Create program.js

---
+++ Java Script Programing Corse(SU)/2. Simple Calculations/2D Rectangle Area/program.js
@@ -0,0 +1,11 @@
+function calculateRectangleArea([arg1, arg2, arg3, arg4]) 
+{
+    let x1 = parseFloat(arg1);
+    let y1 = parseFloat(arg2);
+    let x2 = parseFloat(arg3);
+    let y2 = parseFloat(arg4);
+    let width = Math.max(x1, x2) - Math.min(x1, x2);
+    let height = Math.max(y1, y2) - Math.min(y1, y2);
+    console.log(width * height);
+    console.log(2 * (width + height));
+}
