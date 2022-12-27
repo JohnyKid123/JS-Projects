e931ba0ec9a43a23774699375ccb1fcd3ce5ce69
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-27 19:47:39 +0200

    Create program.js

---
+++ Java Script Programing Corse(SU)/3. Simple Conditions/3 Equal Numbers/program.js
@@ -0,0 +1,8 @@
+function areaOfFigures([arg1, arg2,arg3]) {
+  let num1 = parseInt(arg1);
+  let num2 = parseInt(arg2);
+  let num3 = parseInt(arg3);
+  if(num1==num2 && num1==num3 && num2 == num3)
+    console.log("yes")
+    else console.log("no")
+}
