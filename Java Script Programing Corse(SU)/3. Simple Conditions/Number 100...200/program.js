cdde036a88ba2c1069ec53ff4e138f2c1feb4762
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-27 19:45:03 +0200

    Create program.js

---
+++ Java Script Programing Corse(SU)/3. Simple Conditions/Number 100...200/program.js
@@ -0,0 +1,7 @@
+function Converter(arg1) 
+{
+let num = parseInt(arg1);
+if(num<100) console.log("Less than 100");
+else if(num>=100 && num<=200) console.log("Between 100 and 200");
+else if(num>200) console.log("Greater than 200");
+}
