c4194b6054b273356b6f9faad0d8af35c5691a53
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-27 19:05:59 +0200

    Create program.js

---
+++ Java Script Programing Corse(SU)/3. Simple Conditions/Number 0...9 to Text/program.js
@@ -0,0 +1,24 @@
+function grade([arg1])
+{
+  let num = parseInt(arg1);
+  if (num === 1) 
+  console.log("one");
+  else if (num === 2) 
+  console.log("two");
+  else if (num === 3) 
+  console.log("three");
+  if (num === 4) 
+  console.log("four");
+  else if (num === 5) 
+  console.log("five");
+  else if (num === 6) 
+  console.log("six");
+  if (num === 7) 
+  console.log("seven");
+  else if (num === 8) 
+  console.log("eight");
+  else if (num === 9) 
+  console.log("nine");
+  else if(num>9)
+  console.log("number too big");
+}
