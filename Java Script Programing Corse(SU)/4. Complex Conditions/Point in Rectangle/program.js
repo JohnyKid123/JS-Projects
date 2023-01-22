ffb408a1f0acbcd0040cea4a0f0f458f9b62b309
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-22 20:00:52 +0200

    Create program.js

---
+++ Java Script Programing Corse(SU)/4. Complex Conditions/Point in Rectangle/program.js
@@ -0,0 +1,8 @@
+let x1 = parseFloat(prompt("Enter x1: "));
+let y1 = parseFloat(prompt("Enter y1: "));
+let x2 = parseFloat(prompt("Enter x2: "));
+let y2 = parseFloat(prompt("Enter y2: "));
+let x = parseFloat(prompt("Enter x: "));
+let y = parseFloat(prompt("Enter y: "));
+if (x >= x1 && x <= x2 && y >= y1 && y <= y2) console.log("Inside");
+else console.log("Outside");
