89685ea01478f8767c546fd6ef2adb2a3a2e4ae4
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-22 19:36:37 +0200

    Create program.js

---
+++ Java Script Programing Corse(SU)/4. Complex Conditions/Day of Week/program.js
@@ -0,0 +1,27 @@
+let day = parseInt(prompt("Enter a day number (1-7): "));
+switch(day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Error!");
+        break;
+}
