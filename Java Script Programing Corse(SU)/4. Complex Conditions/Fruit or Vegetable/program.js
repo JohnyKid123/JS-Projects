f0d4eef262155bf237ed686f70a1df5458070a85
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-22 19:40:32 +0200

    Create program.js

---
+++ Java Script Programing Corse(SU)/4. Complex Conditions/Fruit or Vegetable/program.js
@@ -0,0 +1,20 @@
+let product = prompt("Enter a product: ");
+switch (product) {
+    case "banana":
+    case "apple":
+    case "kiwi":
+    case "cherry":
+    case "lemon":
+    case "grapes":
+        console.log("fruit");
+        break;
+    case "tomato":
+    case "cucumber":
+    case "pepper":
+    case "carrot":
+        console.log("vegetable");
+        break;
+    default:
+        console.log("unknown");
+        break;
+}
