fdfb2dcf6a3f85dfe24e63cfca9b471e1dcdfa08
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-24 23:17:46 +0200

    Create program.js

---
+++ Java Script Programing Corse(SU)/Exam Practise Tasks/2.1. Simple Calculations - Exam Problems/Change Tiles/program.js
@@ -0,0 +1,11 @@
+let N = parseFloat(prompt("Enter a number"));
+let W = parseFloat(prompt("Enter a number"));
+let L = parseFloat(prompt("Enter a number"));
+let M = parseFloat(prompt("Enter a number"));
+let O = parseFloat(prompt("Enter a number"));
+let S = N*N;
+let SP = M*O;
+let PP = S - SP;
+let SPP = W*L;
+console.log(PP/SPP);
+console.log((PP/SPP)*0.2);
