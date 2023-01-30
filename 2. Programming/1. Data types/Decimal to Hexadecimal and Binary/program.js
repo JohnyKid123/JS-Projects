e5f509012f61bfeb2adc916692214b3f373ae283
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-30 21:49:23 +0200

    Create program.js

---
+++ 2. Programming/1. Data types/Decimal to Hexadecimal and Binary/program.js
@@ -0,0 +1,5 @@
+const decimalNum = parseInt(prompt("Enter a decimal number: "));
+const hexNum = decimalNum.toString(16).toUpperCase();
+const binaryNum = decimalNum.toString(2);
+console.log(hexNum);
+console.log(binaryNum);
