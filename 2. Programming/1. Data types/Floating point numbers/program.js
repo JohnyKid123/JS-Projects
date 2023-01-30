ef83d005de161920bffba29cd867e633a88da16c
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-30 21:51:25 +0200

    Create program.js

---
+++ 2. Programming/1. Data types/Floating point numbers/program.js
@@ -0,0 +1,6 @@
+const num = parseFloat(prompt("Enter a decimal number: "));
+const num1 = parseFloat(prompt("Enter another decimal number: "));
+const num2 = parseFloat(prompt("Enter another decimal number: "));
+console.log(num);
+console.log(num1);
+console.log(num2);
